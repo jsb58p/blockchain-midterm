43dd2817d57e5f6703e489e3dc3f6988a83aa2d6
madelinebro <mbxkd@umsystem.edu>
2025-10-23 08:55:09 -0500

    Update vite.config.ts

--- didlab-dapp/vite.config.ts
+++ didlab-dapp/vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "node:path";
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 }
+  server: { port: 5173 },
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+      "@lib": path.resolve(__dirname, "src/lib"),
+    },
+  },
 });
+
 
